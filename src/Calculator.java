d150336be74b43543fab8cd5a1cbbd243c0ad1c5
Sayed Khater <khaterkhan@yahoo.com>
2019-01-19 12:32:05 -0500

    write add method

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,9 @@ public class Calculator {
     public static  double add(double num1, double num2){
         return num1+num2;
     }
+    public static double subtract (double num1, double num2){
+        return num1-num2;
+    }
     }
 
 
