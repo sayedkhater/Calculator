4d40b75e6f8d8d955dce4ac9f8d4cada96c9a91a
Sayed Khater <khaterkhan@yahoo.com>
2019-01-19 13:10:44 -0500

    division mehtod

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,10 +4,14 @@ public class Calculator {
     }
     public static double subtract (double num1, double num2){
         return num1-num2;
-        return result;
+
          }
         public static double multiply(double num1, double num2){
-        return num1* num2}
+        return num1* num2;
+    }
+        public static double divide(double num1, double num2){
+        return num1/num2;
+        }
    
     }
 
